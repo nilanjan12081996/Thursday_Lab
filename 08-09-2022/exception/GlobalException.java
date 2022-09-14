3fd4da533a6075d5595889aaa8997d6ed0e4b1b0
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-09-14 15:50:49 +0530

    Create GlobalException.java

---
+++ 08-09-2022/exception/GlobalException.java
@@ -0,0 +1,11 @@
+/*
+ * Author Nilanjan Dasgupta
+ */
+package com.ems.exception;
+//for throw exception
+public class GlobalException extends Exception {
+	public GlobalException(String message) {
+		super(message);
+	}
+
+}
